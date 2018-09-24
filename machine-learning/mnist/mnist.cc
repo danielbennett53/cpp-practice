95f3817d6a3d3064503a294aa09db22af53269c4
Daniel Bennett <daniel.bennett53@gmail.com>
2018-09-23 18:15:21 -0700

    separated files

--- machine-learning/mnist/mnist.cc
+++ machine-learning/mnist/mnist.cc
@@ -1,12 +1,10 @@
-#include <vector>
 #include <fstream>
 #include <iostream>
 #include <Eigen/Dense>
+#include "mnist.h"
 
-#define TRAIN 1
-#define TEST 2
 
-int32_t swap32(int32_t val)
+static int32_t swap32(int32_t val)
 {
   return (val << 24) |
          ((val <<  8) & 0x00ff0000) |
@@ -15,17 +13,17 @@ int32_t swap32(int32_t val)
 }
 
 
-int read_images(Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic>& image_out, Eigen::Matrix<uint8_t,
-  Eigen::Dynamic, 1>& label_out, int flags)
+int read_images(Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic>& image_out,
+  Eigen::Matrix<uint8_t, Eigen::Dynamic, 1>& label_out, int flags)
 {
   std::ifstream img_file, lbl_file;
 
   if (flags & TRAIN) {
-    img_file.open("data/train-images.idx3-ubyte", std::ios::binary);
-    lbl_file.open("data/train-labels.idx1-ubyte", std::ios::binary);
+    img_file.open("mnist/data/train-images.idx3-ubyte", std::ios::binary);
+    lbl_file.open("mnist/data/train-labels.idx1-ubyte", std::ios::binary);
   } else if (flags & TEST) {
-    img_file.open("data/t10k-images.idx3-ubyte", std::ios::binary);
-    lbl_file.open("data/t10k-labels.idx1-ubyte", std::ios::binary);
+    img_file.open("mnist/data/t10k-images.idx3-ubyte", std::ios::binary);
+    lbl_file.open("mnist/data/t10k-labels.idx1-ubyte", std::ios::binary);
   } else {
     std::cerr << "No valid dataset selected!" << std::endl;
   }
@@ -66,44 +64,13 @@ int read_images(Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic>& image_ou
   img_file.seekg(16);
   lbl_file.seekg(8);
 
-  for (int i=0; i<meta_img[1]; ++i)
+  for (uint16_t i=0; i<meta_img[1]; ++i)
   {
     label_out(i) = lbl_file.get();
-    for (int j=0; j<784; ++j)
+    for (int j=0; j<IMG_LEN; ++j)
     {
       image_out(j, i) = (uint8_t)img_file.get();
     }
   }
   return 0;
-}
-
-template <typename Derived>
-void print_image(const Eigen::DenseBase<Derived>& im)
-{
-  for (int i=0; i<28; ++i)
-  {
-    for (int j=0; j<28; ++j)
-    {
-      printf("%3d", im(i*28+j,0));
-    }
-    std::cout << std::endl;
-  }
-}
-
-int main()
-{
-  int idx;
-  Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic> images(1,1);
-  Eigen::Matrix<uint8_t, Eigen::Dynamic, 1> labels(1,1);
-
-  if (read_images(images, labels, TEST) < 0)
-    return -1;
-
-  std::cout << "Choose image index: ";
-  while(std::cin >> idx) {
-    auto test = images.col(idx);
-    print_image(test);
-    std::cout << "number is: " << +labels(idx,0) << std::endl << std::endl;
-    std::cout << "Choose image index: ";
-  }
 }
